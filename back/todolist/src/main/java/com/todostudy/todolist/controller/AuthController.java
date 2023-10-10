d919196c8dd67037f77837c2d98b30489c89041b
DESKTOP-NUOPUIF\ITPS <b44sup97045@gmail.com>
2023-10-10 17:42:02 +0900

    feat: 2023_10_10 v.2

--- back/todolist/src/main/java/com/todostudy/todolist/controller/AuthController.java
+++ back/todolist/src/main/java/com/todostudy/todolist/controller/AuthController.java
@@ -5,10 +5,7 @@ import com.todostudy.todolist.exception.SignupException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
@@ -17,9 +14,10 @@ import java.util.Objects;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
 
-    @PostMapping("/auth/signup")
+    @PostMapping("/signup")
     public ResponseEntity<?> signup(@Valid @RequestBody SignupReqDto signupReqDto, BindingResult bindingResult) {
 
         // AOP
