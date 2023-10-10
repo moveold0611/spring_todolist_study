96c00071226e2f2881fd9eea3c6bfabdd2b0e19a
DESKTOP-NUOPUIF\ITPS <b44sup97045@gmail.com>
2023-10-10 09:44:32 +0900

    first commit

---
+++ back/todolist/src/main/java/com/todolist/todolist/controller/UserController.java
@@ -0,0 +1,6 @@
+package com.todolist.todolist.controller;
+
+public class UserController {
+
+
+}
---
+++ back/todolist/src/main/java/com/todolist/todolist/dto/User.java
@@ -0,0 +1,17 @@
+package com.todolist.todolist.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String email;
+    private String password;
+    private String name;
+
+}
